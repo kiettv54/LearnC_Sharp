c0952a7edfabe74307edb8547f11e3b5df1358bf
= <=>
2020-05-14 11:10:56 +0700

    add ICollection

---
+++ C_Sharp_Advanced/ICollecttion_Csharp/ICollecttion_Csharp/MyArrayList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ICollecttion_Csharp
+{
+	class MyArrayList:ICollection
+	{
+		private object[] Isobject;
+		private int count;
+		private const int maxcount = 100;
+		public MyArrayList()
+		{
+			count = -1;
+			Isobject = new object[maxcount];
+		}
+		public MyArrayList(int count)
+		{
+			this.count = count;
+			Isobject = new object[count];
+		}
+		public MyArrayList(Array array)
+		{
+			array.CopyTo(Isobject, 0);
+			count = array.Length;
+		}
+		public void CopyTo(Array array,int index)
+		{
+			Isobject.CopyTo(array, index);
+		}
+	}
+}
---
+++ C_Sharp_Advanced/ICollecttion_Csharp/ICollecttion_Csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ICollecttion_Csharp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
