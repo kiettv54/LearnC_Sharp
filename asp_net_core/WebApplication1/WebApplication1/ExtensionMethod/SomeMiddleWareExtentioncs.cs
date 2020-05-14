2165518ff92f0fb7e895982eabe2f38f33c7a3c8
= <=>
2020-05-14 23:16:49 +0700

    add new project

---
+++ asp_net_core/WebApplication1/WebApplication1/ExtensionMethod/SomeMiddleWareExtentioncs.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.SampleMiddleware;
+
+namespace WebApplication1.ExtensionMethod
+{
+	public static class SomeMiddleWareExtentioncs
+	{
+		public static IApplicationBuilder UseSimpleMiddleWare(this IApplicationBuilder builder)
+		{
+			return builder.UseMiddleware<MiddleWare>();
+		}
+	}
+}
