7d13129bca5842fe79664799eb413523f249f2cc
= <=>
2020-05-16 09:39:43 +0700

    add Pratice Mvc Asp.net core

---
+++ asp_net_core/MVCWebsite/MVCWebsite/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCWebsite.Models;
+
+namespace MVCWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new IndexModel();
+            model.Message = "Hello from Model";
+            return View(model);
+        }
+    }
+}
